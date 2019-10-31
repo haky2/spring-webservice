0051ba5f541244838c0152d107086c606bcb7bc7
haky <haky2@naver.com>
2019-11-01 01:15:24 +0900

    JPA 이용한 간단한 API 테스트

--- src/main/java/com/haky/webservice/web/WebRestController.java
+++ src/main/java/com/haky/webservice/web/WebRestController.java
@@ -1,12 +1,26 @@
 package com.haky.webservice.web;
 
+import com.haky.webservice.domain.posts.PostsRepository;
+import com.haky.webservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
+
     @GetMapping("/hello")
     public String hello() {
-        return "HelloWorld";
+        return "Hi haky";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
     }
 }
